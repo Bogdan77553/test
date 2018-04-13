7f5faa3d15e72da66562f21470d8e52cadf2bfc4
Bogdan <parfeniuk.bogdan@gmail.com>
2018-04-13 16:55:13 +0300

    finish

--- mylab.cpp
+++ mylab.cpp
@@ -1,5 +1,9 @@
-Devices found: 4
+Devices found: 8
 device [01]: ip: 10.10.10.45 mac: 00:80:e1:38:00:1f hwid: 6e7e8fd4 ldr: 0.1.0 fw: 0.2.0 key: [qwe234f34] dhcp: 1 status: USB_READY
 device [02]: ip: 10.10.10.45 mac: 00:80:e1:38:00:1f hwid: 6e7e8fd4 ldr: 0.1.0 fw: 0.2.0 key: [jk23kj34k] dhcp: 1 status: USB_READY
 device [03]: ip: 10.10.10.45 mac: 00:80:e1:38:00:1f hwid: 6e7e8fd4 ldr: 0.1.0 fw: 0.2.0 key: [] dhcp: 1 status: USB_READY
 device [04]: ip: 10.10.10.45 mac: 00:80:e1:38:00:1f hwid: 6e7e8fd4 ldr: 0.1.0 fw: 0.2.0 key: [] dhcp: 1 status: USB_READY
+device [05]: ip: 10.10.10.45 mac: 00:80:e1:38:00:1f hwid: 6e7e8fd4 ldr: 0.1.0 fw: 0.2.0 key: [jk23kj34k] dhcp: 1 status: USB_READY
+device [06]: ip: 10.10.10.45 mac: 00:80:e1:38:00:1f hwid: 6e7e8fd4 ldr: 0.1.0 fw: 0.2.0 key: [] dhcp: 1 status: USB_READY
+device [07]: ip: 10.10.10.45 mac: 00:80:e1:38:00:1f hwid: 6e7e8fd4 ldr: 0.1.0 fw: 0.2.0 key: [jk23kj34k] dhcp: 1 status: USB_READY
+device [08]: ip: 10.10.10.45 mac: 00:80:e1:38:00:1f hwid: 6e7e8fd4 ldr: 0.1.0 fw: 0.2.0 key: [] dhcp: 1 status: USB_READY
--- workMylab.sh
+++ workMylab.sh
@@ -1,38 +1,14 @@
 #!/bin/sh
 
 countStrings=$(awk '/found/{print $0}' mylab.cpp)
-count=$(echo $countStrings | awk '{aq=$3; printf "%d",aq}')
+N=$(echo $countStrings | awk '{aq=$3; printf "%d",aq}')
 
-echo "" | awk '{printf "{\n\"Board\": [\n"}' > json
-
-i=1
-awk '/device \[/{print $0}' mylab.cpp > temp
-      
-FILE="temp"
-while read line; do
-     echo "{" >> json
-     asd=$(echo "$line" | sed 's/\[\]/0/g') 
-     asd=$(echo $asd | sed 's/\[//g; s/\]//g')
-     
-     echo "$asd"| awk '{
-     a=$2; c=$6; d=$4; k=$14; 
-     printf "\"dev\": \"%s\",\n \"mac\": \"%s\",\n \"ip\": \"%s\",\n\"kay\": \"%s\"\n",a,c,d,k}' >> json
-     
-     printf "}" >> json
-     if [ $i != $count ]; then
-        echo "," >> json
-     fi
-     printf "\n" >> json
-
-     i=$(($i+1))
-
-done < $FILE 
-
-
-#awk '{a=$2; c=$6; d=$4;
-#{printf "{\n\"devNumber\": \"%s\",\n\"mac\": \"%s\",\n\"ip\": \"%s\",\n},",a,c,d}' tmp.txt >> json
-
-echo "" | awk '{printf "\n]\n}\n" 
-}' >> json
-
-#rm -f temp
+awk 'BEGIN {count=0; printf "{\n\"Board\": [\n"}
+END {printf "\n]\n}\n"}
+{sub(/\[\]/,"NULL")}  
+{sub(/\[/,""); sub(/\]/,"")} 
+{sub(/\[/,""); sub(/\]/,"")}  
+{if (/device/)
+{a=$2; c=$6; d=$4; k=$14; 
+printf "{\n\"dev\": \"%s\",\n \"mac\": \"%s\",\n \"ip\": \"%s\",\n\"kay\": \"%s\"\n}",a,c,d,k}} 
+{count = count + 1; if ((count < $N)&&(count>1)){ print(",")}  }  ' mylab.cpp > json
